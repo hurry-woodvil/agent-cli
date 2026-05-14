ea4e38ec3785650f668dcec3add607d074797dcd
hurry-woodvil <hurry.woodvil@gmail.com>
2026-05-14 22:48:19 +0900

    feat: CLIアプリケーションへ引数を渡せるようにする closes #4 closes #6 closes #7

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+import argparse
+
+
 def main():
-    print("Hello World")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("left", type=int)
+    parser.add_argument("right", type=int)
+
+    args = parser.parse_args()
+
+    result = args.left + args.right
+
+    print(f"{args.left} + {args.right} = {result}")
 
 
 if __name__ == "__main__":
