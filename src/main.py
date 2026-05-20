b708a2afeca0e25528feff78ced562ff362e74c5
hurry-woodvil <hurry.woodvil@gmail.com>
2026-05-20 12:03:26 +0900

    feat: Claude API設定値を管理できるようにする closes #19

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import argparse
 
+from src.config import load_settings
+from llm.claude_client import ClaudeLLMClient
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -9,7 +12,11 @@ def main():
 
     prompt = args.prompt
 
+    settings = load_settings()
+
     print(f"prompt: {prompt}")
+    print(f"ANTHROPIC_API_KEY: {settings.anthropic_api_key}")
+    print(f"CLAUDE_MODE: {settings.claude_model}")
 
 
 if __name__ == "__main__":
